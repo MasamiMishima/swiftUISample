a6dff9db137824c8e40f80dc8f1194079230fa7a
MasamiMishima <mishima.masami@willgate.co.jp>
2019-02-20 13:36:15 +0900

    init

---
+++ swiftUISample/swiftUISample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftUISample
+//
+//  Created by 三島 正三 on 2019/02/20.
+//  Copyright © 2019 msm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
